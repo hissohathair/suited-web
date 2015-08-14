c4d1261f84d944f14f73838fda5106409c9577ef
Daniel Austin <daniel.austin@gmail.com>
2015-08-14 12:12:58 +1000

    Switch to generic views. Add tests.

--- getsuited/customers/tests.py
+++ getsuited/customers/tests.py
@@ -1,3 +1,37 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Customer
+
+
+class CustomerMethodTests(TestCase):
+
+    def test_was_updated_recently_future_date(self):
+        """
+        was_modified_recently() should return False for future dates.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_customer = Customer(modified_date=time)
+        self.assertEqual(future_customer.was_updated_recently(), False)
+
+    def test_was_updated_recently_old_date(self):
+        """
+        was_modified_recently() should return False for customers
+        edited longer than 3 days ago.
+        """
+        time = timezone.now() - datetime.timedelta(days=4)
+        old_customer = Customer(modified_date=time)
+        self.assertEqual(old_customer.was_updated_recently(), False)
+
+    def test_was_updated_recently_recent_date(self):
+        """
+        was_modified_recently() should return True for customers
+        edited within last 3 days.
+        """
+        time = timezone.now() - datetime.timedelta(days=2)
+        new_customer = Customer(modified_date=time)
+        self.assertEqual(new_customer.was_updated_recently(), True)
+
+
