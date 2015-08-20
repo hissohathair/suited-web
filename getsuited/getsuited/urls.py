f0194e4b269bda50e945e9b6ee28fcea4dd3348d
Daniel Austin <daniel.austin@gmail.com>
2015-08-20 17:38:21 +1000

    Adding django-allauth

--- getsuited/getsuited/urls.py
+++ getsuited/getsuited/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^customers/', include('customers.urls', namespace="customers")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('allauth.urls')),
 ]
 
