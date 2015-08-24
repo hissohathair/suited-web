a47eb5bb2c6eb9d052978fe1d63caca4e3188937
Daniel Austin <daniel.austin@gmail.com>
2015-08-24 16:46:47 +1000

    Fixing decorators for authentication

--- src/customers/views.py
+++ src/customers/views.py
@@ -3,6 +3,9 @@
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.shortcuts import render
 
 from .models import Customer, Measurement
 from .forms import CustomerForm
@@ -15,11 +18,19 @@ class CustomerList(ListView):
         """Return the most recently updated customers."""
         return Customer.objects.order_by('-modified_date')
 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(CustomerList, self).dispatch(*args, **kwargs)
+
 
 class CustomerCreate(CreateView):
     model = Customer
     template_name = 'customers/create.html'
     form_class = CustomerForm
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(CustomerCreate, self).dispatch(*args, **kwargs)
 
 
 class CustomerDetail(DetailView):
@@ -27,15 +38,27 @@ class CustomerDetail(DetailView):
     template_name = 'customers/detail.html'
     form_class = CustomerForm
 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(CustomerDetail, self).dispatch(*args, **kwargs)
+
 
 class CustomerUpdate(UpdateView):
     model = Customer
     template_name = 'customers/update.html'
     form_class = CustomerForm
 
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(CustomerUpdate, self).dispatch(*args, **kwargs)
+
 
 class CustomerDelete(DeleteView):
     model = Customer
     template_name = 'customers/delete.html'
     success_url = reverse_lazy('customers:index')
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(CustomerDelete, self).dispatch(*args, **kwargs)
 
